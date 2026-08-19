3304f715a525c4f23fa08b282316ce8724c734fe
oviyagaya <oviyabala511@gmail.com>
2026-08-19 17:49:46 +0530

    intial commit

---
+++ src/main/java/com/example/TodoAppplication/TodoAppplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.TodoAppplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppplicationApplication.class, args);
+	}
+
+}
